1e1db02f94a6c06f03438283879aea89a43b30de
vikyath <vikyathshetty11@gmail.com>
2024-09-01 11:27:24 +0530

    latest

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+	res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/',(req,res) =>{
+	res.send('hello vikyath')
+
+})
+
+app.listen(port,() => {
+	console.log(`app listening on port ${port}`)
+})

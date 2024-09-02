f385ced7f893698bd6d3b65b08419b0b3f364a20
Vikyath Shetty B <133582265+vikyathshettyy@users.noreply.github.com>
2024-09-02 18:34:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
+app.use(express.json())
 const port = 3000
 
 app.get('/', (req,res) => {
@@ -8,6 +11,20 @@ app.get('/', (req,res) => {
 
 app.get('/',(req,res) =>{
 	res.send('hello vikyath')
+
+})
+
+app.get('/calculate',(req,res)=> {
+    const principle = parseInt(req.query.p);
+    const interest = parseInt(req.query.i);
+    const years = parseInt(req.query.y);
+    console.log(principle,interest,years);
+    const total = principle + (principle*(interest/100)*years);
+    const amount = total-principle;
+    res.json({
+        total,
+        amount
+    })
 
 })
 
